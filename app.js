7fe0c2851c0c79d42740d10ec7ed0513509ff2d5
vigajolabs <32674459+vigajolabs@users.noreply.github.com>
2018-01-02 20:21:19 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -8,18 +8,96 @@ var FBBotFramework=require('fb-bot-framework');
 var listeningport=process.env.PORT || 4000;
 
 
+
+var currencyobtained=false;
+var countryobtained=false;
+var dollarvalue=0.0;
+var country='';
+
+
+
 var bot= new  FBBotFramework({
-page_token :'EAAdlBWRKZA90BAGi2KZCa0aAxWB07eeJsQ8rBi5HmdzEr8rQPnN8E3IixpJ6uACMxyAW2wlZALVqTZBBvy8EfYVTQTX1lxOwvLwW0DP0zPuZCbhm0tJUU5BQHCZAhtGVPbwznkK36mr5Huu7ZC6k3ZCJUGadvqXiTdUAH3BioHPs2koH0acQN42X',  //Esto es de la pagina de Facebook
+page_token :'EAAdlBWRKZA90BAGC36HlZBQ7N4K4vf6eKj9maRB5V9LhyhBBgPDxFY7gYkz0CFlF8S8dUH35wWGP9NNlZB09u1BqBwn4m9M7yevmsD7RZCCXZBejD2Tt097N3BOjcEwimDzoKwtK32swZC3ADaHBQ94gTZBrrjqAAigIuBZAwoqZByCqs8ZBGIYsX4',  //Esto es de la pagina de Facebook
 verify_token :'vigajolabs'
 });
+
+
+CurrencyConvertor = function(userId, msg){
+
+  var orgtext = msg;
+
+  if(!isNumeric(orgtext)){
+  var message = orgtext.toLowerCase();
+  }
+  else {
+    message = orgtext;
+  }
+  //var frmuser = session.message.user.name;
+
+
+  if(message === 'hi' || message === 'hello' || message === 'hey'){
+    bot.sendTextMessage(userId,'Hello.I am your curency convertor BOT.');
+  }
+  else if(message === 'canada' || message === 'india' || message === 'euro'){
+    countryobtained = true;
+    country=message;
+  }
+  else if(isNumeric(message)){
+  dollarvalue=message;
+  currencyobtained=true;
+  }
+
+  if(currencyobtained &&  !countryobtained){
+    bot.sendTextMessage(userId,'Please enter the country to convert');
+  }
+  if(!currencyobtained && countryobtained){
+    bot.sendTextMessage(userId,'Please enter the currency in USD ' + dollarvalue + ' ' + message);
+  }
+  if(!currencyobtained && !countryobtained){
+    bot.sendTextMessage(userId,'Please enter currency in USD to convert');
+  }
+
+  if (currencyobtained && countryobtained) {
+
+    countryobtained=false;
+    currencyobtained=false;
+
+  var convertedvalue=0.0;
+
+    switch(country){
+      case 'canada':
+        convertedvalue = dollarvalue * 1.25;
+        bot.sendTextMessage(userId,' You requested to convert to Canandian currency and the value is ' + convertedvalue);
+        break;
+      case 'india':
+          convertedvalue = dollarvalue * 64.67;
+          bot.sendTextMessage(userId,' You requested to convert to Indian currency and the value is ' + convertedvalue);
+          break;
+      case 'euro':
+              convertedvalue = dollarvalue * 0.85;
+              bot.sendTextMessage(userId,' You requested to convert to Euro and the value is ' + convertedvalue);
+              break;
+
+        }
+
+        bot.sendQuickReplies(userId, ' Please rate my service', quickreply);
+
+  }
+
+};
 
 app.use('/fb/msg', bot.middleware());
 
 
 bot.on('message',function(userId,msg){
-	bot.sendTextMessage(userId,'Hi, I am copy cat :' + msg);
+	
+	CurrencyConvertor(userId,msg);
 
 });
+
+function isNumber(n){
+	return lisNan(parseFloat(n)) && isFinite(n);
+}
 
 app.get('/',function(req,res){
 	res.send("facebook BOT listening...");
